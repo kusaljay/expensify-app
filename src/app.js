35e1bf2c47e981d2330a99cfb2024cf936376e91
kusaljay <kufe28@gmail.com>
2020-02-28 05:56:38 +1100

    Setup react-router-dom, Test BrowserRouter and Routes

--- src/app.js
+++ src/app.js
@@ -1,6 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-ReactDOM.render(<p>Boilerplate</p>, document.getElementById('app'));
+const ExpenseDahsboardPage = () => (
+  <div>
+    This is Expense Dahsboard Page
+  </div>
+);
+
+const routes = (
+  <BrowserRouter>
+    <Route path="/" component={ExpenseDahsboardPage} />
+  </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById('app'));
