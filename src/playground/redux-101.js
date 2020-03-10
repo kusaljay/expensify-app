7b097c3c8c477d2d59c1487d11f71fc638fbafe1
kusaljay <kufe28@gmail.com>
2020-03-10 14:01:46 +1100

    Actions 101

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,7 +1,55 @@
 import { createStore } from 'redux';
 
-const store = createStore((state = { count: 10 }) => {
-  return state;
+const store = createStore((state = { count: 0 }, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return {
+        count: state.count + 1
+      }
+
+    case 'DECREMENT':
+      return {
+        count: state.count - 1
+      }
+
+    case 'RESET':
+      return {
+        count: 0
+      }
+    
+    default:
+      return state;
+  }
+  
+// ----- Same as above switch statement. Switch is preferred over If/else and easily scalable -----
+/*   if(action.type === 'INCREMENT') {
+    return {
+      count: state.count + 1
+    }
+  } 
+  else {
+    return state;
+  } */
+  
 });
+
+// Action - An object that gets sent to the store
+store.dispatch({
+  type: 'INCREMENT'
+});
+
+store.dispatch({
+  type: 'INCREMENT'
+});
+
+store.dispatch({
+  type: 'RESET'
+});
+
+store.dispatch({
+  type: 'DECREMENT'
+});
+
+
 
 console.log(store.getState());
